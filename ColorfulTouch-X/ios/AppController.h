5d140578971972746c59a6c8a4f7790d15f6316b
mintyopera <redopera.saladfestival@gmail.com>
2013-03-29 19:17:05 +0900

    Initial Commit

---
+++ ColorfulTouch-X/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  ColorfulTouch_XAppController.h
+//  ColorfulTouch-X
+//
+//  Created by t.k on 13/03/29.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ ColorfulTouch-X/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ColorfulTouch_XAppController.h
+//  ColorfulTouch-X
+//
+//  Created by t.k on 13/03/29.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
